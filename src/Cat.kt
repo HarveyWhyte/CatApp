5c5ec027b3b72828cdc8bbfb410b8b8f975a3e95
HARVEY WHYTE <0whyth98@solent.ac.uk>
2025-10-01 14:25:22 +0100

    change eat method to take in an amount

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,11 +1,19 @@
 class Cat (val name: String, var age: Int, var weight: Int) {
 
-    fun walk() {
-        weight--
+    public fun walk(distance: Int) : Boolean
+    {
+        if(weight - distance >= 5) {
+            weight -= distance
+            return true
+        }
+        return false
     }
 
-    fun eat() {
-        weight++
+    fun eat(amount: Int) {
+        weight += amount
+        if(weight > 20) {
+            weight = 20
+        }
     }
 
     override fun toString() : String {
